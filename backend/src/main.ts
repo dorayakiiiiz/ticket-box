03ac0e5a5c2e7cb494fd9cf6b14dc22793b01a47
dorayakiiiiz <travansy2305@gmail.com>
2026-06-06 08:49:43 +0700

    enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,7 +14,11 @@ async function bootstrap() {
   // bufferLogs: true giữ các log bootstrap lại, flush sau khi Pino sẵn sàng
   app.useLogger(app.get(Logger));
 
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.CLIENT_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
